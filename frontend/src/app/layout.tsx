e2774bc3f8c965036931bae5c5afc9e4398d8d2e
depper-IA <deploy@virtual-tryon.com>
2026-05-04 12:47:25 -0500

    chore: add Meta Pixel tracking code

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -159,6 +159,32 @@ style={
             src={`https://www.googletagmanager.com/gtag.js?id=${GTM_ID}`}
           />
         )}
+
+        {/* Meta Pixel Code */}
+        <Script id="meta-pixel" strategy="afterInteractive">
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '1496054805555549');
+            fbq('track', 'PageView');
+          `}
+        </Script>
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: 'none' }}
+            src="https://www.facebook.com/tr?id=1496054805555549&ev=PageView&noscript=1"
+            alt=""
+          />
+        </noscript>
+        {/* End Meta Pixel Code */}
         <ThemeProvider>
           <ExitIntentProvider>
             <Suspense fallback={null}>
